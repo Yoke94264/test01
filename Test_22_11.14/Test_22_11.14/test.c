b992f84a31e342d0e56802cf9a5c9192dcfbc68c
zhangshangwei <2698806639@qq.com>
2022-11-14 22:39:52 +0800

    提交代码

---
+++ Test_22_11.14/Test_22_11.14/test.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//鸡兔同笼
+
+int main()
+{
+	int c,t;
+	for (int c = 0; c <= 35; c++)
+	{
+		for (t = 0; t <= 35; t++)
+		{
+			if (35 ==t+c && c * 2 + t * 4 == 96)
+			{
+				printf("鸡有%d只，兔子有%d只\n", c, t);
+			}
+		}
+	}
+	return 0;
+}
