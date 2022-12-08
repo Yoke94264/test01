7ad9a3bae8c086c1277aea53471a076707c61c01
Yoke94264 <2698806639@qq.com>
2022-12-08 12:52:04 +0800

    提交代码

---
+++ Test_12.8/Test_12.8/test.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <assert.h>
+
+
+//º¯ÊýÄ£Äâstrlen
+int my_strlen(const char* des)
+{
+	assert(des != NULL);
+	int i = 0;
+	while (*des++)
+	{
+		i++;
+	}
+	return i;
+}
+
+int main()
+{
+	char ch[] = "zhangsan";
+	printf("%d", my_strlen(ch));
+	return 0;
+}
